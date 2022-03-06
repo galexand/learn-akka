30fd995fca05bff523c6d4c4a9a8a53aa9f762a6
galexand <galexand@adobe.com>
2022-03-06 12:08:11 +0200

    First Actor using Behavior<String>

---
+++ src/main/java/com/galexand/string/actor/Main.java
@@ -0,0 +1,15 @@
+package com.galexand.string.actor;
+
+import akka.actor.typed.ActorSystem;
+
+public class Main {
+
+    public static void main(String[] args) {
+       ActorSystem<String> actorSystem = ActorSystem.create(FirstSimpleBehavior.create(),"FirstActorSystem");
+       actorSystem.tell("say hello");
+       actorSystem.tell("who are you");
+       actorSystem.tell("create a child");
+       actorSystem.tell("This is the second message.");
+    }
+
+}
